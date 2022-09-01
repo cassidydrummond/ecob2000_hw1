28b310947d19f6fd355af19c799c761009f9cb25
cassidydrummond <112599304+cassidydrummond@users.noreply.github.com>
2022-08-31 23:22:39 -0400

    Update ecob2000_lecture1.R

--- ecob2000_lecture1.R
+++ ecob2000_lecture1.R
@@ -5,7 +5,7 @@
 #I think it's interesting that the average age (both for males and females) of respondents was in their mid 50s. With increased skeptism surrounding COVID and government agencies, I'm surprised the average age was of an older generation.
 #I found the difference between those vaccinated and those not to be interestingly large; As the gap within US is about 15% (and the data shows 11.5%). Although, I am curious when this dataset was created and who conducted the survey. I think those factors would impact the audience participating as well as acknowledge accessibility issues.
 
-#1 Adeel Arshid and Ksenia Guseinova
+#1 Adeel Arshid and Kseniia Huseinova
 
 R version 4.1.3 (2022-03-10) -- "One Push-Up"
 Copyright (C) 2022 The R Foundation for Statistical Computing
